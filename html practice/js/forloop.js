715fa3e26e2884ffdc952eee31ed1821e9289798
Manal Mukarram <manalmukarram18@gmail.com>
2023-01-09 20:29:36 +0530

    practised forloop in js

---
+++ html practice/js/forloop.js
@@ -0,0 +1,9 @@
+// Program to add first n natural numbers
+let sum =0 
+let n = prompt("enter n")
+n= Number.parseInt(n)
+for (let i=0; i < n; i++){
+    sum += i+1
+console.log((i+1, "+"))
+}
+console.log(sum)
